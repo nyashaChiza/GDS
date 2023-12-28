1fa303440ccb31bf26ec827fc01203f84448e887
NyashaChiza <nchizampeni@gmail.com>
2023-12-28 22:07:10 +0200

    savepoint

--- gds/urls.py
+++ gds/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include("debug_toolbar.urls")),
+    path('accounts/', include('allauth.urls')),
 ]
